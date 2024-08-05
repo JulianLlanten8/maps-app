dfcb743823b04f4017849f11881a4e4288183273
Julian Llanten <hugojulians@gmail.com>
2024-08-04 20:29:57 -0500

    update: from webpack to vue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-createApp(App).use(store).use(router).mount('#app')
+import mapboxgl from 'mapbox-gl';
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoianVsaWFubGxhbnRlbjgiLCJhIjoiY2t5dnJocjIxMDB2azJxcDlrbDd4eHE0aiJ9.yNyda6_cCY0hw0U-_FEbzw';
+if(!navigator.geolocation){
+  alert("Tu navegador no soporta el geolocation");
+  throw new Error("Tu navegador no soporta el geolocation");
+}
+createApp(App).use(store).use(router).mount('#app');
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,13 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+// My custom modules
+import placesModule from './places';
+import { PlacesState } from './places/state';
+export interface StateInterface {
+  places : PlacesState,
+}
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+export default createStore<StateInterface>({
   modules: {
+    places: placesModule
   }
-})
+});
